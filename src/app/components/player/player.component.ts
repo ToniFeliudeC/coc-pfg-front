315c3a5c0757d7c831e5bf5b426816462b669d30
ToniFeliudeC <afeliusalas@cifpfbmoll.eu>
2024-05-29 19:09:46 +0200

    player detail view advanced

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit, inject } from "@angular/core";
 import { PlayersService } from "../../services/player.service";
 import { ActivatedRoute } from "@angular/router";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss'
 })
