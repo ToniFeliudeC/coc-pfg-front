a2293e37a9fa5dd43f69df7ab8ee2c96d48158a7
ToniFeliudeC <afeliusalas@cifpfbmoll.eu>
2024-06-09 16:09:23 +0200

    basic clan filter added

--- src/app/components/clans-view/clans-view.component.ts
+++ src/app/components/clans-view/clans-view.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, model } from '@angular/core';
+import { ClansService } from '../../services/clan.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-clans-view',
@@ -7,6 +9,68 @@ import { Component } from '@angular/core';
   templateUrl: './clans-view.component.html',
   styleUrl: './clans-view.component.scss'
 })
-export class ClansViewComponent {
+export class ClansViewComponent implements OnInit {
+  private clansService: ClansService = inject(ClansService);
+  clans: any[] = [];
+  tagInput: any;
+  clan: any;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.loadCachedClans();
+  }
+
+  searchClan(tag: string) {
+    this.clansService.getClan(tag).subscribe(
+        (clan: any) => {
+            if (clan && clan.reason !== 'notFound') {
+              this.clans.push(clan);
+              this.cacheClans();
+            }
+            
+        }
+    );
+  }
+
+  viewClan(tag: string) {
+      this.router.navigate(['/clans', tag]);
+  }
+
+  cacheClans() {
+    try {
+      sessionStorage.setItem('cachedClans', JSON.stringify(this.clans));
+    } catch (error) {
+      console.error('Error caching clans:', error);
+    }
+  }
+
+  loadCachedClans() {
+    try {
+      const cachedClans = sessionStorage.getItem('cachedClans');
+      if (cachedClans) {
+        this.clans = JSON.parse(cachedClans) || [];
+      }
+    } catch (error) {
+      console.error('Error loading cached clans:', error);
+    }
+  }
+
+  emptyCachedClans() {
+    try {
+      const cachedClans = sessionStorage.getItem('cachedClans');
+      if (cachedClans) {
+        this.clans = [];
+        sessionStorage.clear();
+      }
+    } catch (error) {
+      console.error('Error loading cached clans:', error);
+    }
+  }
+
+  // Función para eliminar el primer carácter del tag
+  getTrimmedTag(tag: string): string {
+    return tag ? tag.substring(1) : '';
+  }
 
 }
